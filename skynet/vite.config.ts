9b2a764b4075f5221c6335f2353f553facd5004c
Timo Lambing <timo@skynetintelligence.com>
2023-02-17 15:45:18 +0200

    Initial Commit

---
+++ skynet/vite.config.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Skynet Intelligence",
+        short_name: "Skynet",
+        description: "The Talent Management Platform",
+        theme_color: "#0779c4",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
